8a2bcf928876b2cebf9d86f5aabef7769f8137e2
Pavel Isaev <pavel.isaev@wolt.com>
2020-02-18 08:58:00 +0200

    Add go home button to notfound component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import Home from './Home';
 
 const NotFound = () => (
-  <div>404 Not found</div>
+  <div className='center'>
+    <div>404 Not found</div>
+    <Link to='/'>
+      <button className='btn home-redirect'  type="button">
+        Go to home page
+      </button>
+    </Link>
+  </div>
 );
 
 export default NotFound;
